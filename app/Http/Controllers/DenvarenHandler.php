b92afd564fd019e6a6136d740b1f62b905ecf34f
VladimirDubinin <ro100v4anin@yandex.ru>
2026-01-15 13:22:59 +0300

    Добавлено меню с командами

--- app/Http/Controllers/DenvarenHandler.php
+++ app/Http/Controllers/DenvarenHandler.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use DefStudio\Telegraph\Facades\Telegraph;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
 use Illuminate\Support\Stringable;
 
@@ -12,7 +13,7 @@ class DenvarenHandler extends WebhookHandler
         $this->chat->html('<b>Нет такой команды:</b> ' . $text)->send();
     }
 
-    public function start(): void
+    protected function start(): void
     {
         $this->reply('Приветствую нового пользователя!');
     }
@@ -20,5 +21,29 @@ class DenvarenHandler extends WebhookHandler
     protected function handleChatMessage(Stringable $text): void
     {
         $this->chat->html('"' . $text . '" - это то, что я так хотел услышать!')->send();
+    }
+
+    protected function com(): void
+    {
+        Telegraph::registerBotCommands([
+            'list' => 'Список дат',
+            'add' => 'Добавить дату',
+            'delete' => 'Удалить дату',
+        ])->send();
+    }
+
+    protected function list(): void
+    {
+        $this->reply('Вывожу список дат...');
+    }
+
+    protected function add(): void
+    {
+        $this->reply('Добавляю дату...');
+    }
+
+    protected function delete(): void
+    {
+        $this->reply('Удаляю дату...');
     }
 }
