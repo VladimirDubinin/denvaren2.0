e54f609d99e635503403461076255d420f3aa251
VladimirDubinin <ro100v4anin@yandex.ru>
2026-04-02 20:33:21 +0300

    Исправлен баг в роутере

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Src\TelegramBot\Application\Controllers\WebhookController;
+use App\TelegramBot\Application\Controllers\WebhookController;
 
 Route::post('/telegram/webhook', WebhookController::class);
